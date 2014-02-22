2efc9a437bd7bc37826c449b705619cc2cbbd5a1
Mike Buhot <m.buhot@gmail.com>
2014-02-23 08:54:55 +1000

    Problem 9 - special pythagorean tiplet, math solution
     - Euclids formula for triples
     - constrain by a+b+c = 1000
     - reduces to find factors of 500 in the form m(m+n) where m > n
     - m = 20, n = 5

--- problem-009.py
+++ problem-009.py
@@ -15,5 +15,29 @@ def BruteForceSolution():
       c = 1000 - b - a
       if a*a + b*b == c*c:
         return a*b*c
+ 
+
+#Euclids formula:
+# for all m and n with m > n. The formula states that the integers
+# a = m^2 - n^2, b = 2mn, c = m^2 + n^2
+# are a pythagorean triple
+#
+# We have 
+#   1000 = a + b + c 
+#        = 2m^2 + 2mn
+# => 500 = m^2 + mn
+#        = m(m + n)
+# factors of 500 are: 1, 2, 4, 5, 10, 20, 25, 50, 100, 125, 250, 500
+# only 20 x 25 fits: m(m+n), for m > n: m = 20, n = 5
+def MathSolution():
+  m = 20
+  n = 5
+  a = m*m - n*n
+  b = 2 * m *n
+  c = m*m + n*n 
+  assert(a*a + b*b == c*c)
+  assert(a + b + c == 1000)
+  return a*b*c
 
 print("Brute force solution:", BruteForceSolution())
+print("Math solution: ", MathSolution())
