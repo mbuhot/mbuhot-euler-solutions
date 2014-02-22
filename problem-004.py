7d1a29b8ec7e5d1730596a495878c9c091f85b0b
Mike Buhot <m.buhot@gmail.com>
2014-02-22 20:22:41 +1000

    cleanup problem 4
     - slight optimization
     - spelling of palindrome

--- problem-004.py
+++ problem-004.py
@@ -5,7 +5,7 @@
 #A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 #Find the largest palindrome made from the product of two 3-digit numbers.
 
-def isPalendrome(s):
+def isPalindrome(s):
   return s == s[::-1]
   
 def BruteForceSolution(numDigits):
@@ -15,13 +15,13 @@ def BruteForceSolution(numDigits):
   for i in range(lower, upper):
     for j in range(i, upper):
       prod = i * j
-      if isPalendrome(str(prod)) and prod > result:
+      if prod > result and isPalindrome(str(prod)):
         result = prod
   print("Largest palindrome from product of 2", numDigits, "digit numbers is:", result)
   return result
-    
+
 # Test isPalendrome helper function
-assert(isPalendrome("abcdefgfedcba"))
+assert(isPalindrome("abcdefgfedcba"))
 
 # Test brute force solution with example from problem
 assert(BruteForceSolution(2) == 9009)
