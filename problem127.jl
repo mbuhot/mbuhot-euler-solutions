75c2f45350704359235d177c2744f57437adb8dd
Mike Buhot <m.buhot@gmail.com>
2014-06-01 08:19:38 +1000

    Problem 127 - ABC-hits (optimized version)
    Improves performace from >30 seconds to ~1 second
     - Remove all usages of tuples
     - inline summation with generation
     - remove additional GCD calls
     - use sortperm instead of sort(by) with lambda

--- problem127.jl
+++ problem127.jl
@@ -20,10 +20,11 @@ It turns out that abc-hits are quite rare and there are only thirty-one abc-hits
 Find ∑c for c < 120000.
 """
 
+# calculate rad(x) for x=1:n using prime sieve 
 function radsieve(n :: Int)
-  r = ones(Int, n)
+  r = ones(Int, n)   
   for i = 2:n
-    if r[i] == 1
+    if r[i] == 1     
       for j = i:i:n
         r[j] *= i
       end
@@ -32,35 +33,40 @@ function radsieve(n :: Int)
   return r
 end
 
-function abchits(cmax :: Int)
+# Calculate sum(c) for c < cmax and a,b,c is an ABC-Hit
+function abchits(cmax :: Int = 120000)
+  # Pre-compute rad, and sort 1:cmax by rad
   r = radsieve(cmax)
-  s = sort(collect(enumerate(r)), by=x->x[2], alg=MergeSort)	
+  p = sortperm(r)
 
+  csum = 0
   for c = 3:cmax
     radc = r[c]
+
+    # rad(a*b*c) == rad(a) * rad(b) * rad(c) when a,b,c are coprime
+    # rad(a)*rad(b)*rad(c) < c implies rad(a) < c / (rad(c) * rad(b))
+    # using 2 as a lower bound for rad(b), max value for rad(a) is c / (2 * rad(c))
     radamax = div(c, radc * 2)
     amax = div(c, 2)
-    for (a, rada) in s
-      if rada > radamax break end
-      if a  > amax continue end
+
+    # search the sorted list for suitable rad(a), a
+    for i = 1:cmax
+      a = p[i]
+      rada = r[a]
+      if rada > radamax break end # stop searching once rad(a) is too large
+      if a  > amax continue end   # skip over a that are too large
       b = c - a
       radb = r[b]
-      if rada * radb * radc < c &&
-         gcd(a, c) == 1 &&
-         gcd(a, c) == 1 && 
-         gcd(b, c) == 1
-        produce((a,b,c))
+
+      # gcd(a,c) == 1 implies gcd(a,b) == 1 and gcd(b,c) == 1
+      # if there were some d that divides a and b, then d would also divide (a+b)
+      if rada * radb * radc < c && gcd(a, c) == 1 
+        csum += c
       end
     end
   end
-end
 
-function abchitcsum(cmax :: Int)
-  total = 0
-  for (a,b,c) in @task abchits(cmax)
-    total += c
-  end
-  return total
+  return csum
 end
-
+	
 end
