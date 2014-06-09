82fce641f8ef788f480ebe9591d169d5ccddb477
Mike Buhot <m.buhot@gmail.com>
2014-06-09 16:24:13 +1000

    Problem 131 - Prime cuban partnership, proof added.
     - Added derivation in comments for the solution.
     - First establish that n is a cube, and p+n is a cube.
     - Implies p is a difference of cubes
     - But given p is prime, and the difference of cubes formula:
       y^3 - x^3 = (y-x)(y^2 + xy + x^2),
       then y-x must be 1.
     - Hence p is the difference of consecutive cubes.
     - Therefore, code searches for prime consecutive cube differences.
     - Code only trivially changed to match the math in comments

--- problem131.jl
+++ problem131.jl
@@ -12,21 +12,34 @@ How many primes below one million have this remarkable property?
 
 # By exhaustive search, I found the first few primes are 7, 19, 37, 61
 # These are known as Cuban primes: primes of the form n^3 - (n-1)^3
-
+#
+# Insight after reading solutions from the ProjectEuler forums:
+#
+# n^3 + p * n^2 = k^3
+# n^3 * (p/n + 1) = k^3
+# n^3 * ((p+n)/n) = k^3
+# n * cuberoot((p+n)/n) = k
+# n * cuberoot(p+n) / cuberoot(n) = k
+# For the cuberoot expressions to have integer results, p+n and n must be cubes.
+# let n = x^3, p+n = y^3
+# p = y^3 - x^3
+# p = (y - x)(y^2 + xy + x^2)
+# But p is required to be prime, so (y-x) must be 1 - otherwise (y-x) would be a factor of p.
+# Therefore, p is the difference of consecutive cubes:
+# p = y^3 - (y-1)^3
 function cubans(n = 1000000)
-  x = 0
-  for i = 2:n
-    p = i^3 - (i-1)^3
-    if p > n break
-    elseif isprime(p) x += 1 end
+  c = 0
+  for y = 2:Inf
+    p = y^3 - (y-1)^3
+    if p > n 
+      return c 
+    elseif isprime(p) 
+      c += 1 
+    end
   end
-  return x
 end
 
 using Base.Test
-
 @test cubans(100) == 4
-
-
-                                                                                                                                                                                                                                                                
+                                                                                                                                                                                                                                                             
 end
