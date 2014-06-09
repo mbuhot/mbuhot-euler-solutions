861219e35aef68d2515ce81302db6ce2dc6b7d41
Mike Buhot <m.buhot@gmail.com>
2014-06-09 18:40:20 +1000

    Problem 132 - Large repunit factors (optimized)
     - Rather than finding A(p) for all primes, just
       test if 10^n == 1 mod 9p
     - Reduces running time from >50 seconds to < 1 second.

--- problem132.jl
+++ problem132.jl
@@ -9,17 +9,17 @@ For example, R(10) = 1111111111 = 11×41×271×9091, and the sum of these prime 
 Find the sum of the first forty prime factors of R(10^9).
 """
 
-using problem129
-
-# when k divides n, then R(k) divides R(n), which implies that the prime factors of R(k) also divide R(n)
-# The function below uses this to find the first Repunit that each prime divides using problem129.A(p)
-# It then checks if that k divides the required n.
+# Optimized search
+# R(n) = (10^n - 1)/9
+# R(n) = 0 mod p
+# (10^n - 1) / 9 = 0 mod p
+# 10^n - 1 = 0 mod 9p
+# 10^n = 1 mod 9p
+# so search for p satisfying 10^n = 1 mod 9p using Julias powermod function 
 function search(n = 10^9, facs = 40, maxprime = 200000)
   factors = Int[]
   for p in primes(maxprime)
-    if 10 % p == 0 continue end
-    k = problem129.A(p)
-    if n % k == 0
+    if powermod(10, n, 9*p) == 1
       push!(factors, p)
       if length(factors) == facs
         return sum(factors)
